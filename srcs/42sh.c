9c6ecc33c5cae84555abbe8704aef6c6a6aca631
sur4c1 <94838879+sur4c1@users.noreply.github.com>
2026-08-17 20:09:33 +0200

    DEV: basic main loop

---
+++ srcs/42sh.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   42sh.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yyyyyy <yyyyyy@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/17 18:46:19 by yyyyyy            #+#    #+#             */
+/*   Updated: 2026/08/17 19:54:24 by yyyyyy           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int
+main(int argc, char *argv[])
+{
+	char *line;
+	char  buffer[2048];
+
+	while (1)
+	{
+		line = ft_readline(">", buffer, sizeof(buffer));
+		if (!line)
+			break;
+		write(1, line, ft_strlen(line));
+	}
+	(void) argc;
+	(void) argv;
+}
